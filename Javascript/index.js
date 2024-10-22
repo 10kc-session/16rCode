650ef0d04f0ae3d11cd7915119544aef93ad0ff9
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-10-22 15:52:21 +0530

    Filter Data

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,30 +1,45 @@
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
+let filterData = document.getElementById("btn-2");
 
 btn.onclick = async () => {
     let response = await fetch("https://fakestoreapi.com/products");
     let data = await response.json();
     alert("Data Fetched");
     localStorage.setItem("data", JSON.stringify(data));
-    displayData();
+    displayData(data);
 }
-function displayData() {
-    output.innerHTML = "";
+
+filterData.onclick = () => {
     let data = JSON.parse(localStorage.getItem("data")) || [];
-    if (data.length === 0) {
-        output.innerHTML = "No Data Available";
+    if (data.length > 0) {
+        data = data.filter(obj => obj["category"] === "electronics");
+        displayData(data);
     } else {
-        data.forEach(obj => {
-            let information = document.createElement("div");
-            information.className = "cards"
-            information.innerHTML =
-                `<p><b>Title : </b>${obj["title"]}</p>
-                <p><b>Price : </b>${obj["price"]}</p>
-                <p><b>Description : </b>${obj["description"]}</p>
-                <p><b>Category : </b>${obj["category"]}</p>`;
-            output.appendChild(information);
-        });
+        output.innerHTML = "No Data Available";
     }
 }
 
-window.onload = displayData;
+function displayData(data) {
+    output.innerHTML = "";
+    data.forEach(obj => {
+        let information = document.createElement("div");
+        information.className = "cards"
+        information.innerHTML =
+            `<p><b>Title : </b>${obj["title"]}</p>
+            <p><b>Price : </b>${obj["price"]}</p>
+            <p><b>Description : </b>${obj["description"]}</p>
+            <p><b>Category : </b>${obj["category"]}</p>`;
+        output.appendChild(information);
+    });
+}
+
+window.onload = () => {
+    let data = JSON.parse(localStorage.getItem("data")) || [];
+    if (data.length > 0) {
+        displayData(data);
+    } else {
+        output.innerHTML = "No Data Available";
+    }
+
+}

e0c03d0862b9094ed2a23d675351e69d03610657
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-10-22 15:41:56 +0530

    LocalStorage - 2

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,24 +1,29 @@
-let input = document.getElementById("input");
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
-btn.onclick = () => {
-    if (input.value === "") {
-        alert("Enter the Value !!!");
-        return;
-    }
-    let students = JSON.parse(localStorage.getItem("students")) || [];
-    students.push(input.value);
-    localStorage.setItem("students", JSON.stringify(students));
-    input.value = "";
+
+btn.onclick = async () => {
+    let response = await fetch("https://fakestoreapi.com/products");
+    let data = await response.json();
+    alert("Data Fetched");
+    localStorage.setItem("data", JSON.stringify(data));
     displayData();
 }
-
 function displayData() {
-    let students = JSON.parse(localStorage.getItem("students"));
-    if (!students) {
+    output.innerHTML = "";
+    let data = JSON.parse(localStorage.getItem("data")) || [];
+    if (data.length === 0) {
         output.innerHTML = "No Data Available";
     } else {
-        output.innerHTML = students.join(", ");
+        data.forEach(obj => {
+            let information = document.createElement("div");
+            information.className = "cards"
+            information.innerHTML =
+                `<p><b>Title : </b>${obj["title"]}</p>
+                <p><b>Price : </b>${obj["price"]}</p>
+                <p><b>Description : </b>${obj["description"]}</p>
+                <p><b>Category : </b>${obj["category"]}</p>`;
+            output.appendChild(information);
+        });
     }
 }
 
